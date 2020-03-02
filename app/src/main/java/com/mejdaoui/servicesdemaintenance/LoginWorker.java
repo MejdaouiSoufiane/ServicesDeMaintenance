b8576b96e878dc01f94c88a8840bd538e6d57caa
btissamnachit <46863416+btissamnachit@users.noreply.github.com>
2020-03-03 00:36:40 +0100

    login ihm client / fonctionnaire

--- app/src/main/java/com/mejdaoui/servicesdemaintenance/LoginWorker.java
+++ app/src/main/java/com/mejdaoui/servicesdemaintenance/LoginWorker.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class LoginClient extends AppCompatActivity {
+public class LoginWorker extends AppCompatActivity {
     ImageView sback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_client);
+        setContentView(R.layout.activity_login_worker);
         sback = (ImageView)findViewById(R.id.sinb);
         sback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(LoginClient.this,MainActivity.class);
+                Intent it = new Intent(LoginWorker.this,MainActivity.class);
                 startActivity(it);
             }
         });
--- app/src/main/java/com/mejdaoui/servicesdemaintenance/MainActivity.java
+++ app/src/main/java/com/mejdaoui/servicesdemaintenance/MainActivity.java
@@ -2,15 +2,52 @@ package com.mejdaoui.servicesdemaintenance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView sinClient;
+    TextView sinWorker;
+    LinearLayout circle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Firebase Connected .. ", Toast.LENGTH_SHORT).show();
+
+        circle = (LinearLayout)findViewById(R.id.circle);
+        sinClient = (TextView)findViewById(R.id.sin2);
+        sinWorker = (TextView)findViewById(R.id.sin3);
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this,LoginClient.class);
+                startActivity(it);
+
+            }
+        });
+        sinClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(
+                        MainActivity.this,LoginClient.class);
+                startActivity(it);
+            }
+        });
+
+        sinWorker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(
+                        MainActivity.this,LoginWorker.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
