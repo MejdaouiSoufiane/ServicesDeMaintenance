e9e082090610915d1163e93303b4a9bcccad7789
btissamnachit <46863416+btissamnachit@users.noreply.github.com>
2020-03-02 23:09:51 +0100

    login ihm

---
+++ app/src/main/java/com/mejdaoui/servicesdemaintenance/LoginClient.java
@@ -0,0 +1,14 @@
+package com.mejdaoui.servicesdemaintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_client);
+    }
+}
